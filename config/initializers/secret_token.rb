94b9f869274a850436d43dca36f361b4853cca34
calpar <calpar@gmail.com>
2012-04-17 13:39:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chocolate::Application.config.secret_token = '58d0ff16b4a192572c4deb6b0b580559cba23a794d7bbd97a3f85e1d3a7a962f45dd439b9eec7b516ba945b020777aa3e4cad8c944ef310107c4551fd7e861b6'
